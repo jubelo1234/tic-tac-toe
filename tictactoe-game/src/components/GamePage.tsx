e66d9f25e9b65c503ad07ad2ba951b0eacaa1057
jubelo1234 <ohootuberecaiah@gmail.com>
2024-02-27 19:47:31 +0100

    done for today

--- tictactoe-game/src/components/GamePage.tsx
+++ tictactoe-game/src/components/GamePage.tsx
@@ -4,8 +4,29 @@ import oIcon from "../assets/icon-o.svg";
 import resIcon from "../assets/icon-restart.svg";
 import Square from "./Square";
 import ScoreUi from "./ScoreUi";
+import { useState } from "react";
 
 export default function GamePage() {
+  type Player = "X" | "O";
+  type Board = Player | null;
+
+  const winningCombos: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const initialBoard: Board[] = Array(9).fill(null);
+
+  const [board, setBoard] = useState<Board[]>(initialBoard);
+  const [currentPlayer, setCurrentPlayer] = useState<Player>('X');
+  const [winner, setWinner] = useState<Player | 'Draw' | null>(null);
+
   return (
     <div className="w-full max-w-[550px] tab:max-w-[460px] mx-auto">
       <div className="flex justify-between items-center">
@@ -13,34 +34,44 @@ export default function GamePage() {
           <img src={logo} alt="logo" className="h-[2rem]" />
         </div>
         <div className="flex justify-center items-center pb-[4px] gap-[0.8125rem] rounded-[0.625rem] h-[2.5rem] tab:h-[3.25rem] w-[6rem] tab:w-[8.75rem] bg-semi-dark-navy shadow-turn">
-          <img src={xIcon} alt="turns" className=" size-[16px] tab:size-[20px]" />
-          <p className=" leading-none font-medium text-[0.875rem] uppercase text-silver-cl">Turn</p>
+          <img
+            src={xIcon}
+            alt="turns"
+            className=" size-[16px] tab:size-[20px]"
+          />
+          <p className=" leading-none font-medium text-[0.875rem] uppercase text-silver-cl">
+            Turn
+          </p>
         </div>
         <button className="size-[2.5rem] tab:size-[3.25rem] grid place-items-center cursor-pointer rounded-[10px] shadow-res bg-silver-cl">
-          <img src={resIcon} alt="restart" className="size-[16px] tab:size-[20px]" />
+          <img
+            src={resIcon}
+            alt="restart"
+            className="size-[16px] tab:size-[20px]"
+          />
         </button>
       </div>
       <div className="mt-[4rem] tab:mt-[2rem] space-y-[1.25rem]">
         <div className="flex items-center justify-between gap-[3vw] tab:gap-[15px]">
-            <Square/>
-            <Square/>
-            <Square/>
+          <Square />
+          <Square />
+          <Square />
         </div>
         <div className="flex items-center justify-between gap-[3vw] tab:gap-[15px]">
-            <Square/>
-            <Square/>
-            <Square/>
+          <Square />
+          <Square />
+          <Square />
         </div>
         <div className="flex items-center justify-between gap-[3vw] tab:gap-[15px]">
-            <Square/>
-            <Square/>
-            <Square/>
+          <Square />
+          <Square />
+          <Square />
         </div>
       </div>
       <div className="flex items-center justify-between gap-[3vw] mt-[1.19rem] tab:gap-[15px]">
-        <ScoreUi/>
-        <ScoreUi/>
-        <ScoreUi/>
+        <ScoreUi />
+        <ScoreUi />
+        <ScoreUi />
       </div>
     </div>
   );
