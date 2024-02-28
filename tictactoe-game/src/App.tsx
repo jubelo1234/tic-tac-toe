3f735e121be5755ff153ae2bc39d3a629d2524eb
jubelo1234 <116165926+jubelo1234@users.noreply.github.com>
2024-02-28 20:03:59 +0100

    new ui components

--- tictactoe-game/src/App.tsx
+++ tictactoe-game/src/App.tsx
@@ -4,20 +4,35 @@ import GamePage from "./components/GamePage";
 import Home from "./components/Home";
 import RestartUi from "./components/RestartUi";
 import WinCard from "./components/WinCard";
+import TieCard from "./components/TieCard";
 
 function App() {
-
   const [homePage, setHomePage] = useState<boolean>(true);
   const [singlePlayerMode, setSinglePlayerMode] = useState<boolean>(false);
   const [player1, setPlayer1] = useState<string>("O");
+  const [modal, setModal] = useState<string | null>("tie");
 
   return (
-    <div className="px-[6vw] py-10 flex items-center justify-center bg-dark-navy min-h-screen min-w-screen">
-      {homePage ? <Home playerOne={setPlayer1} setPage={setHomePage} singlePlayer={setSinglePlayerMode}/> : <GamePage/>}
-      {/* <div className="w-full absolute top-0 h-full">
-       <RestartUi/>
-      </div> */}
-    </div>
+    <>
+      <div className="px-[6vw] py-10 flex items-center justify-center bg-dark-navy min-h-screen min-w-screen">
+        {homePage ? (
+          <Home
+            playerOne={setPlayer1}
+            setPage={setHomePage}
+            singlePlayer={setSinglePlayerMode}
+          />
+        ) : (
+          <GamePage />
+        )}
+      </div>
+      {modal && (
+        <div className="w-full absolute top-0 h-full">
+          {modal === "restart" && <RestartUi />}
+          {modal === "tie" && <TieCard />}
+          {modal !== "restart" && modal !== "tie" && <WinCard />}
+        </div>
+      )}
+    </>
   );
 }
 
